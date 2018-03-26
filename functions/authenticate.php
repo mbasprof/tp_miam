e767d16f46e521cfa540ee95bd3822fe3ef33c64
mbasprof <0rn1t0Github>
2018-03-26 12:19:24 -0400

    ajout de la fonction pour authentifier l'utilisateur

---
+++ functions/authenticate.php
@@ -0,0 +1,24 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: mbastide
+     * Date: 2018-03-26
+     * Time: 12:05
+     */
+
+//on reçoit le username et le password en paramètres
+    function authenticate($username, $pwd)
+    {
+        //tableau des utilisateurs
+        $users = array(
+            'Marion' => 'a589e9efdfe4754a29d176b13c9122ca',
+            'Gilles' => 'b6a615cbb437caa5a0cc0ff2d833eb3a',
+
+
+        );
+
+        return array_key_exists($username, $users)  //l'utilisateur existe dans le tableau des utilisateurs
+            && ($users[$username] === md5($pwd);// on compare le mot de passe saisi au mot de passe encrypté
+
+    }
+
